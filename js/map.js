69fa2700b111da8715d781999c91ee192dc6d677
dimbi <dimasrinarso2003@yahoo.com>
2015-05-08 23:51:39 -0400

    online csv reference

--- js/map.js
+++ js/map.js
@@ -405,7 +405,7 @@ var popup = new L.Popup({ autoPan: false });
 	  //    {date:parseDate("25-Apr-12"),close:99.00}
 	  //];
 
-	  d3.tsv("data/weekend.csv", function(error, data) {
+	  d3.tsv("http://dimbi.github.io/tipmebig/data/weekend.csv", function(error, data) {
 		  data.forEach(function(d) {
 		  });
 
