435c49a528dc770ce33216d057153cf7516f2c50
dimbi <dimasrinarso2003@yahoo.com>
2015-05-09 10:28:58 -0400

    add interactive template

--- js/map.js
+++ js/map.js
@@ -356,6 +356,150 @@ var popup = new L.Popup({ autoPan: false });
   }
 
 
+	var n = 3, // number of layers
+	    m = 24, // number of samples per layer
+	    stack = d3.layout.stack(),
+	    layers = stack(d3.range(n).map(function() { return bumpLayer(m, .1); })),
+	    yGroupMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y; }); }),
+	    yStackMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
+
+	var margin = {top: 20, right: 20, bottom: 40, left: 20},
+	    width = 460 - margin.left - margin.right,
+	    height = 220 - margin.top - margin.bottom;
+
+   	var color = d3.scale.ordinal()
+    	.range(["#990033","#006666","#CCFF66"]);
+
+	var x = d3.scale.ordinal()
+	    .domain(d3.range(m))
+	    .rangeRoundBands([0, width], .08);
+
+	var y = d3.scale.linear()
+	    .domain([0, yStackMax])
+	    .range([height, 0]);
+
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom")
+	    .ticks(4);
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+		.ticks(4);
+
+	var svg = d3.select(".legendBox").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var layer = svg.selectAll(".layer")
+	    .data(layers)
+	  .enter().append("g")
+	    .attr("class", "layer")
+	    .style("fill", function(d, i) { return color(i); });
+
+	var rect = layer.selectAll("rect")
+	    .data(function(d) { return d; })
+	  .enter().append("rect")
+	    .attr("x", function(d) { return x(d.x); })
+	    .attr("y", height)
+	    .attr("width", x.rangeBand())
+	    .attr("height", 0);
+
+	rect.transition()
+	    .delay(function(d, i) { return i * 10; })
+	    .attr("y", function(d) { return y(d.y0 + d.y); })
+	    .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+
+	svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	      .style("stroke","grey")
+	      .style("fill","grey")
+	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Tip ($)")
+	      .style("stroke","grey")
+	      .style("fill","grey");
+
+	svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis)
+  	      .style("stroke","grey")
+          .style("fill","grey");  
+
+
+	d3.selectAll("input").on("change", change);
+
+	var timeout = setTimeout(function() {
+	  d3.select("input[value=\"grouped\"]").property("checked", true).each(change);
+	}, 2000);
+
+	function change() {
+	  clearTimeout(timeout);
+	  if (this.value === "grouped") transitionGrouped();
+	  else transitionStacked();
+	}
+
+	function transitionGrouped() {
+	  y.domain([0, yGroupMax]);
+
+	  rect.transition()
+	      .duration(500)
+	      .delay(function(d, i) { return i * 10; })
+	      .attr("x", function(d, i, j) { return x(d.x) + x.rangeBand() / n * j; })
+	      .attr("width", x.rangeBand() / n)
+	    .transition()
+	      .attr("y", function(d) { return y(d.y); })
+	      .attr("height", function(d) { return height - y(d.y); });
+	}
+
+	function transitionStacked() {
+	  y.domain([0, yStackMax]);
+
+	  rect.transition()
+	      .duration(500)
+	      .delay(function(d, i) { return i * 10; })
+	      .attr("y", function(d) { return y(d.y0 + d.y); })
+	      .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+	    .transition()
+	      .attr("x", function(d) { return x(d.x); })
+	      .attr("width", x.rangeBand());
+	}
+
+	// Inspired by Lee Byron's test data generator.
+	function bumpLayer(n, o) {
+
+	  function bump(a) {
+	    var x = 1 / (.1 + Math.random()),
+	        y = 2 * Math.random() - .5,
+	        z = 10 / (.1 + Math.random());
+	    for (var i = 0; i < n; i++) {
+	      var w = (i / n - y) * z;
+	      a[i] += x * Math.exp(-w * w);
+	    }
+	  }
+
+	  var a = [], i;
+	  for (i = 0; i < n; ++i) a[i] = o + o * Math.random();
+	  for (i = 0; i < 5; ++i) bump(a);
+	  return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
+	}
+
+
+
+
+
+
+
+/*
 
 //area chart Cluster 0
 //------------
@@ -457,7 +601,6 @@ var popup = new L.Popup({ autoPan: false });
 //area chart Cluster 1
 //------------
 
-/*
 	var svg = d3.select(map.getPanes().overlayPane).append("svg"),
 	g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
@@ -644,11 +787,9 @@ var popup = new L.Popup({ autoPan: false });
         .style("fill","grey");    
 
 	  });
-
-*/
 //end area chart
 
-
+*/
 
 
   
